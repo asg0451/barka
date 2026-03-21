b5c017a04c5e6ccc7ad53d4c61c23587c84a960c
Miles Frankel <miles.frankel@cockroachlabs.com>
2026-03-21 03:26:42 -0400

    j

--- jepsen/barka/src/jepsen/barka/core.clj
+++ jepsen/barka/src/jepsen/barka/core.clj
@@ -1,10 +1,10 @@
 (ns jepsen.barka.core
   "Jepsen test for barka distributed log.
 
-   Workload: queue semantics per partition.
-   - :produce appends a value to the log
-   - :consume reads the next value from the log
-   - Checker validates total ordering: no lost, duplicated, or reordered values."
+   Workload: single-partition log correctness.
+   - :produce appends a uniquely identified value to the log
+   - :consume reads the next unconsumed value from the log (shared offset)
+   - Checker validates: every produced value consumed exactly once, in order."
   (:require [clojure.tools.logging :refer [info warn]]
             [jepsen
              [checker :as checker]
@@ -17,35 +17,46 @@
             [jepsen.barka.db :as db]))
 
 (defn log-checker
-  "Checks that every produced value is consumed exactly once, in order."
+  "Checks that every produced value is consumed exactly once, in produce order."
   []
   (reify checker/Checker
     (check [this test history opts]
-      (let [produced  (->> history
-                           (filter #(and (= :produce (:f %)) (= :ok (:type %))))
-                           (mapv :value))
-            consumed  (->> history
-                           (filter #(and (= :consume (:f %)) (= :ok (:type %))))
-                           (mapv :value))
-            lost      (remove (set consumed) produced)
-            ;; Check ordering: consumed values should appear in same relative order as produced
-            prod-order (into {} (map-indexed (fn [i v] [v i]) produced))
-            indices    (keep prod-order consumed)
-            ordered?   (= indices (sort indices))
-            valid?     (and (empty? lost) ordered?)]
-        {:valid?     valid?
-         :produced   (count produced)
-         :consumed   (count consumed)
-         :lost       (count lost)
-         :in-order?  ordered?}))))
+      (let [produced   (->> history
+                            (filter #(and (= :produce (:f %)) (= :ok (:type %))))
+                            (mapv :value))
+            consumed   (->> history
+                            (filter #(and (= :consume (:f %)) (= :ok (:type %))))
+                            (mapv :value))
+            duplicates (- (count consumed) (count (distinct consumed)))
+            lost       (vec (remove (set consumed) produced))
+            unexpected (vec (remove (set produced) consumed))
+            ;; Check ordering: consumed should be a subsequence of produced
+            prod-idx   (zipmap produced (range))
+            indices    (keep prod-idx consumed)
+            ordered?   (= (seq indices) (seq (sort indices)))
+            valid?     (and (empty? lost)
+                            (empty? unexpected)
+                            (zero? duplicates)
+                            ordered?)]
+        {:valid?      valid?
+         :produced    (count produced)
+         :consumed    (count consumed)
+         :duplicates  duplicates
+         :lost        (count lost)
+         :lost-values (take 10 lost)
+         :unexpected  (count unexpected)
+         :in-order?   ordered?}))))
+
+(def next-value (atom 0))
 
 (defn barka-client
-  "A Jepsen client for barka's distributed log."
-  [conn next-offset]
+  "A Jepsen client for barka's distributed log.
+   All clients share a consume-offset atom that only advances on successful reads."
+  [conn consume-offset]
   (reify client/Client
     (open! [this test node]
-      (let [c (barka/open "127.0.0.1" db/control-port)]
-        (barka-client c (atom 0))))
+      (barka-client (barka/open "127.0.0.1" db/control-port)
+                    (:consume-offset test)))
 
     (setup! [this test])
 
@@ -57,10 +68,11 @@
             (assoc op :type :ok :offset offset))
 
           :consume
-          (let [values (barka/consume! conn "default" 0 @next-offset 1)]
+          (let [off    @consume-offset
+                values (barka/consume! conn "default" 0 off 1)]
             (if (seq values)
-              (do (swap! next-offset inc)
-                  (assoc op :type :ok :value (first values)))
+              (do (compare-and-set! consume-offset off (inc off))
+                  (assoc op :type :ok :value (parse-long (first values))))
               (assoc op :type :fail :error :empty))))
         (catch Exception e
           (assoc op :type :info :error (.getMessage e)))))
@@ -74,27 +86,34 @@
 (defn barka-test
   "Constructs a Jepsen test map for barka."
   [opts]
-  (merge tests/noop-test
-         opts
-         {:name      "barka"
-          :db        (db/db opts)
-          :client    (barka-client nil nil)
-          :checker   (checker/compose
-                       {:log      (log-checker)
-                        :timeline (timeline/html)})
-          :generator (gen/phases
-                       (->> (gen/mix [(fn [_ _] {:type :invoke :f :produce :value (rand-int 10000)})
-                                      (fn [_ _] {:type :invoke :f :consume})])
-                            (gen/stagger 1/10)
-                            (gen/clients)
-                            (gen/time-limit 10))
-                       (gen/log "Draining...")
-                       (->> (fn [_ _] {:type :invoke :f :consume})
-                            (gen/stagger 1/10)
-                            (gen/clients)
-                            (gen/time-limit 5)))
-          :nodes     ["n1"]
-          :ssh       {:dummy? true}}))
+  (reset! next-value 0)
+  (let [consume-offset (atom 0)]
+    (merge tests/noop-test
+           opts
+           {:name           "barka"
+            :consume-offset consume-offset
+            :db             (db/db opts)
+            :client         (barka-client nil nil)
+            :checker        (checker/compose
+                              {:log      (log-checker)
+                               :timeline (timeline/html)})
+            :generator      (gen/phases
+                              (->> (gen/mix [(fn [_ _]
+                                              {:type :invoke
+                                               :f :produce
+                                               :value (swap! next-value inc)})
+                                             (fn [_ _]
+                                               {:type :invoke :f :consume})])
+                                   (gen/stagger 1/10)
+                                   (gen/clients)
+                                   (gen/time-limit 10))
+                              (gen/log "Draining...")
+                              (->> (fn [_ _] {:type :invoke :f :consume})
+                                   (gen/stagger 1/10)
+                                   (gen/clients)
+                                   (gen/time-limit 5)))
+            :nodes          ["n1"]
+            :ssh            {:dummy? true}})))
 
 (def cli-opts
   [[nil "--barka-bin PATH" "Path to barka binary"
